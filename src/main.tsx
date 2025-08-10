0be44495db8e2d63215184f4b055e120cb1f6d6f
Gustavo Souza <gustavo.oliveira.souza@outlook.com>
2025-08-10 19:29:18 -0300

    replace import

--- src/main.tsx
+++ src/main.tsx
@@ -19,7 +19,7 @@ declare module "@tanstack/react-router" {
 }
 
 // Import your Publishable Key
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = process.env.VITE_CLERK_PUBLISHABLE_KEY
 
 if (!PUBLISHABLE_KEY) {
   throw new Error('Add your Clerk Publishable Key to the .env file')
